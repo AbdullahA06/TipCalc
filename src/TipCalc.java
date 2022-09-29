3c6b3f52b9f521f7877af63f4fe69ff2bfd218ae
AbdullahA06 <aabdullaev0283@bths.edu>
2022-09-28 22:37:37 -0400

    Added comments to increase readability of code

--- src/TipCalc.java
+++ src/TipCalc.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class TipCalc {
     public static void main(String[] args) {
 
+        // three user inputs, the bill, tip %, and number of people
         Scanner s = new Scanner(System.in);
         System.out.print("Enter the bill amount: ");
         double totalBill=s.nextDouble();
@@ -11,11 +12,13 @@ public class TipCalc {
         int numPeople=s.nextInt();
         System.out.println();
 
+        // variables to store important values that we can get from the user inputs. ex: finalBill is the bill + tax
         double totalTip= (totalBill*tipPercent)/100;
         double finalBill=totalBill+totalTip;
         double soloBill=finalBill/numPeople;
         double tipPerPerson=totalTip/numPeople;
 
+        // print statements that properly and concisely output the desired values along with text to make it easier to understand what is what.
         System.out.print("In total your party has tipped $");
         System.out.format("%.2f%n",totalTip);
         System.out.println();
